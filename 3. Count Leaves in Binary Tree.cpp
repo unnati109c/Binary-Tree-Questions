d69c06e96ed736e5dacc0651ec1fb70b6d4400b1
unnati109c <67646140+unnati109c@users.noreply.github.com>
2021-04-21 17:10:02 +0530

    Classical Problem

---
+++ 3. Count Leaves in Binary Tree.cpp
@@ -0,0 +1,59 @@
+/*
+          1
+        /    \
+       2      3
+      / \    / \
+     4   5  6   7
+    /     \    /
+   8       9  10
+  /
+ 11
+
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+struct node
+{
+	int data;
+	struct node *left, *right;
+
+	node(int val)
+	{
+		data=val;
+		left=right=NULL;
+	}
+};
+
+int countLeaves(struct node* root)
+{
+	if(root == NULL)
+		return 0;
+	if(root->left==NULL and root->right==NULL)
+		return 1;
+	return countLeaves(root->left)+countLeaves(root->right);
+}
+
+int main()
+{
+	struct node* root = new node(1);
+
+	root->left = new node(2);
+	root->right = new node(3);
+
+	root->left->left = new node(4);
+	root->left->right = new node(5);
+
+	root->right->left = new node(6);
+	root->right->right = new node(7);
+
+	root->left->left->left = new node(8);
+	root->left->right->right = new node(9);
+	root->right->right->left = new node(10);
+
+	root->left->left->left->left = new node(11);
+
+	cout<<"Leaves in Binary Tree : "<<countLeaves(root);
+	return 0;
+}
