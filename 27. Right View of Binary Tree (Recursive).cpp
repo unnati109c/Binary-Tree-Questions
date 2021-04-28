86cc62d294c640d9299f59ecb1e18e7d239fd988
unnati109c <67646140+unnati109c@users.noreply.github.com>
2021-04-28 20:01:23 +0530

    Classical Problem*

---
+++ 27. Right View of Binary Tree (Recursive).cpp
@@ -0,0 +1,73 @@
+/* 
+		      1
+		2           3
+	   4 5       6     7
+			   8		9
+			   			 10
+*/
+#include<bits/stdc++.h>
+using namespace std;
+
+struct node
+{
+	int data;
+	struct node *left, *right;
+
+	node(int val)
+	{
+		data = val;
+		left = NULL;
+		right = NULL;
+	}
+};
+
+struct node* insert(int val)
+{
+		return new node(val);
+}
+
+int max_level = 0;
+void RightViewHelper(vector<int>&vec, struct node* root, int level)
+{
+	if(root == NULL)
+		return ;
+	if(max_level < level)
+	{
+		vec.push_back(root->data);
+		max_level = level;
+	}
+	RightViewHelper(vec, root->right, level+1);
+	RightViewHelper(vec, root->left, level+1);
+}
+
+vector<int> RightView(struct node* root)
+{
+	max_level=0;
+	vector<int>v;
+	RightViewHelper(v, root, 1);
+	return v;
+}
+
+int main()
+{
+	struct node* root = insert(1);
+	root->left = insert(2);
+	root->right = insert(3);
+
+	root->left->left = insert(4);
+	root->left->right = insert(5);
+
+	root->right->left = insert(6);
+	root->right->right = insert(7);
+
+	root->right->left->left = insert(8);
+	root->right->right->right = insert(9);
+
+	root->right->right->right->right = insert(10);
+	
+	vector<int> ans = RightView(root);
+	for(auto i : ans)
+		cout<<i<<" "; 
+	return 0;
+}	
+
